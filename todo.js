80ceecc14727f581c922847d35fd5017ec00d8f8
sumbu viny <sumbuviny3@gmail.com>
2023-01-16 20:54:20 -0800

    code quality
    A naming convention: Arranged
    Some tags: replaced
    some values and names: changed

--- todo.js
+++ todo.js
@@ -1,19 +1,19 @@
 window.addEventListener('load', () => {
      // step 1 set and get a username or a single string from a database
     // create a global variable
-    todos = JSON.parse(localStorage.getItem('todos')) || []; // if there is any todo's saved in our local storage we're going to get those
+    todoProject = JSON.parse(localStorage.getItem('todos')) || []; // if there is any todo's saved in our local storage we're going to get those
       
-    // create nameInput, we're going to get the nameInput
-    const nameInput = document.querySelector('#name');  
-    // create newTodoForm
-    const newTodoForm = document.querySelector('#todo-form');
-     // create username, we're going to get the username
-    const username = localStorage.getItem('username') || '';
+    // create name, we're going to get the nameInput
+    const name = document.querySelector('#username');  
+    // create TodoForm
+    const todoForm = document.querySelector('#todoForm');
+     // create user, we're going to get the username
+    const user = localStorage.getItem('username') || '';
      // whatever is in our local storage it will pull it here
-    nameInput.value = username;
+    name.value = user;
       
     // change made
-    nameInput.addEventListener('change', e=> {
+    name.addEventListener('change', e=> {
          // we're actually going to set an item in our local storage called username and we're set to the value there 
         localStorage.setItem('username', e.target.value); 
 
@@ -21,70 +21,70 @@ window.addEventListener('load', () => {
      
     // step 2 get JSON code from it 
 
-    newTodoForm.addEventListener('submit', e=>{
+    todoForm.addEventListener('submit', e=>{
         e.preventDefault();
 
-          //create todo which is going to have our content, category,..we're going to get our content from the form element
+          //create todoElem which is going to have our content, option,..we're going to get our content from the form element
 
-        const todo={
+        const todoElem ={
             content: e.target.elements.content.value, // e.target which is going to be the form we submitted,.. .value to get the actual value
-            category: e.target.elements.category.value,
+            option: e.target.elements.option.value,
             done: false,
-            createdAt: new Date().getTime()
+            MadeAt: new Date().getTime()
         }
            // add the new todo to our array or global variable
-        todos.push(todo);
+        todoProject.push(todoElem);
            // save our local storage item
-        localStorage.setItem('todos', JSON.stringify(todos));
+        localStorage.setItem('todos', JSON.stringify(todoProject));
            // reset our target
         e.target.reset();
 
-        DisplayTodos(); // call this function
+        showTodos(); // call this function
     })
-    DisplayTodos();
+    showTodos();
 })
 
 // create a function, to display items on screeen
-function DisplayTodos(){
+function showTodos(){
      // create todo list
-    const todoList = document.querySelector('#todo-list');
+    const todoList = document.querySelector('#todoList');
 
     todoList.innerHTML = ''; //clear all  of the elements
       // loop through every single todo in our todo's array
-    todos.forEach(todo => {
+    todoProject.forEach(todoElem => {
         const todoItem = document.createElement('div');
-        todoItem.classList.add('todo-item');
+        todoItem.classList.add('todoItem');
 
         const label = document.createElement('label');
         const input = document.createElement('input');
-        const span = document.createElement('span');
+        const strong = document.createElement('strong');
         const content = document.createElement('div');
         const actions = document.createElement('div');
         const edit = document.createElement('button');
         const del = document.createElement('button');
 
         input.type = 'checkbox';
-        input.checked = todo.done;
-        span.classList.add('Menu');
+        input.checked = todoElem.done;
+        strong.classList.add('menu');
 
           // add menu class
-        if(todo.category == 'drink'){
-            span.classList.add('drink');
+        if(todoElem.option == 'drink'){
+            strong.classList.add('drink');
         }else{
-            span.classList.add('food');  
+            strong.classList.add('food');  
         }
 
-        content.classList.add('todo-content')
+        content.classList.add('todoContent')
         actions.classList.add('actions');
         edit.classList.add('edit');
         del.classList.add('delete');
 
-        content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+        content.innerHTML = `<input type="text" value="${todoElem.content}" readonly>`;
         edit.innerHTML = 'Edit';
         del.innerHTML = 'Delete';
 
         label.appendChild(input);
-        label.appendChild(span);
+        label.appendChild(strong);
         actions.appendChild(edit);
         actions.appendChild(del);
         todoItem.appendChild(label);
@@ -93,22 +93,22 @@ function DisplayTodos(){
 
         todoList.appendChild(todoItem);
 
-        if (todo.done){
+        if (todoElem.done){
             todoItem.classList.add('done');
         }
 
         input.addEventListener('click', e=>{
-             todo.done = e.target.checked;
+             todoElem.done = e.target.checked;
 
-             localStorage.setItem('todos', JSON.stringify(todos));
+             localStorage.setItem('todos', JSON.stringify(todoProject));
 
-             if(todo.done){
+             if(todoElem.done){
                 todoItem.classList.add('done');
              }else{
                 todoItem.classList.remove('done');
              }
 
-             DisplayTodos();
+             showTodos();
         })
           // edit button
         edit.addEventListener('click', e=>{
@@ -118,17 +118,17 @@ function DisplayTodos(){
 
              input.addEventListener('blur', e=>{
                 input.setAttribute('readonly',true);
-                todo.content = e.target.value;
-                localStorage.setItem('todos', JSON.stringify(todos));
-                DisplayTodos();
+                todoElem.content = e.target.value;
+                localStorage.setItem('todos', JSON.stringify(todoProject));
+                showTodos();
             })
         })
 
         // delete button
         del.addEventListener('click', e=>{
-                 todos = todos.filter(t => t!== todo);
-                 localStorage.setItem('todos', JSON.stringify(todos));
-                 DisplayTodos();
+                 todoProject = todoProject.filter(todo => todo!== todoElem);
+                 localStorage.setItem('todos', JSON.stringify(todoProject));
+                 showTodos();
         })
     })
 }
